593ae067fb58846e542ed61cd807c8d8777b95e5
Amanda3282 <135915348+AmandaW0143@users.noreply.github.com>
2024-12-04 01:22:41 -0500

    add collection images and js

--- js/story.js
+++ js/story.js
@@ -9,3 +9,28 @@ function option(element) {
 let content = document.getElementById("content").innerHTML;
 arr[arr.length] = content;
 localStorage.setItem("array", JSON.stringify(arr));
+
+/* collection functions */
+function panda() {
+  localStorage.setItem("panda", "obtained");
+}
+
+function lantern() {
+  localStorage.setItem("lantern", "obtained");
+}
+
+function ribbon() {
+  localStorage.setItem("ribbon", "obtained");
+}
+
+function bell() {
+  localStorage.setItem("bell", "obtained");
+}
+
+function bamboo() {
+  localStorage.setItem("bamboo", "obtained");
+}
+
+function poppy() {
+  localStorage.setItem("poppy", "obtained");
+}
