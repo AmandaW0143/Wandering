e4e181bca0b56998334eb9d0b369f490ce17e59b
Amanda3282 <135915348+AmandaW0143@users.noreply.github.com>
2024-12-04 01:27:33 -0500

    remove console log

--- js/print.js
+++ js/print.js
@@ -1,6 +1,5 @@
 window.onload = function () {
   let story = JSON.parse(localStorage.getItem("array"));
-  console.log(story);
   for (let i = 0; i < story.length; i++) {
     document.getElementById("your-story").innerHTML += story[i];
   }
